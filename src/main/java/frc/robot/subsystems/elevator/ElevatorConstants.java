90a2f39718d4e6b93df393c6538712cfec484ea9
rabaaks <parabaakashs@gmail.com>
2025-01-12 10:58:26 -0500

    e

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,9 +1,20 @@
 package frc.robot.subsystems.elevator;
 
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
 
 public class ElevatorConstants {
     public static final DCMotor motor = DCMotor.getNEO(2);
 
-    public static final double elevatorMass = 10.0;
+    public static final double mass = 8.956;
+    public static final double radius = 0.918;
+    public static final double gearing = 2.25;
+
+    public static final double maxHeight = Units.inchesToMeters(60);
+    public static final double minHeight = Units.inchesToMeters(35);
+    public static final double startingHeight = minHeight;
+
+    public static final double p = 0.0;
+    public static final double i = 0.0;
+    public static final double d = 0.0;
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -2,11 +2,32 @@ package frc.robot.subsystems.elevator;
 
 import static frc.robot.subsystems.elevator.ElevatorConstants.*;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
-    private ElevatorSim siem = new ElevatorSim(null, null, 0, 0, false, 0, null);
     private ElevatorSim sim = new ElevatorSim(
-        LinearSystemId.createElevatorSystem(motor, 0, 0, 0)
+        LinearSystemId.createElevatorSystem(motor, mass, radius, gearing),
+        motor,
+        minHeight,
+        maxHeight,
+        true,
+        startingHeight
+    );
+
+    private PIDController feedback = new PIDController(p, i, d);
+    private double feedforward = 0.0;
+    
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        double velocity = sim.getPositionMeters();
+
+        double voltage = feedforward + feedback.calculate
+        sim.setInputVoltage()
+    }
+
+    public void setPosition(double position) {
+
+    }
 }
